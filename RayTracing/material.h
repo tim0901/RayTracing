4cec840a6038f74e5b4f11da41c4622f2db74ea6
Alex Richardson <alexander1.richardson@gmail.com>
2018-08-19 23:59:56 +0100

    Dielectrics support

--- RayTracing/material.h
+++ RayTracing/material.h
@@ -3,6 +3,24 @@
 #include "ray.h"
 #include "hitable.h"
 
+float schlick(float cosine, float ref_idx) {
+	float r0 = (1 - ref_idx) / (1 + ref_idx);
+	r0 = r0 * r0;
+	return r0 + (1 - r0)*pow((1 - cosine), 5);
+}
+
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted) {
+	vec3 uv = unit_vector(v);
+	float dt = dot(uv, n);
+	float discriminant = 1.0 - ni_over_nt * ni_over_nt*(1 - dt * dt);
+	if (discriminant > 0) {
+		refracted = ni_over_nt * (uv - n * dt) - n * sqrt(discriminant);
+		return true;
+	}
+	else
+		return false;
+}
+
 vec3 reflect(const vec3& v, const vec3& n) {
 	return v - 2 * dot(v, n)*n;
 }
@@ -48,4 +66,59 @@ public:
 	}
 	vec3 albedo;
 	float fuzz;
+};
+
+//Defines properties of a dielectric material (glasses)
+class dielectric : public material {
+public:
+	float ref_idx;
+	dielectric(const vec3& a, float ri) : albedo(a), ref_idx(ri) {}
+	vec3 albedo;
+	
+	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+		vec3 outward_normal;
+		vec3 reflected = reflect(r_in.direction(), rec.normal);
+		float ni_over_nt;
+		attenuation = vec3(1.0, 1.0, 1.0);
+		vec3 refracted;
+		float reflect_prob;
+		float cosine;
+		attenuation = albedo;
+
+		// when ray shoot through object back into vacuum,
+		// ni_over_nt = ref_idx, surface normal has to be inverted
+
+		if (dot(r_in.direction(), rec.normal) > 0) {
+			outward_normal = -rec.normal;
+			ni_over_nt = ref_idx;
+            cosine = dot(r_in.direction(), rec.normal) / r_in.direction().length();
+            cosine = sqrt(1 - ref_idx*ref_idx*(1-cosine*cosine));
+		}
+		else {
+			outward_normal = rec.normal;
+			ni_over_nt = 1.0 / ref_idx;
+			cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+		}
+
+		//refracted ray exists
+		if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+			reflect_prob = schlick(cosine, ref_idx);
+		}
+		//refracted ray does not exist
+		else {
+			//total internal reflection
+			reflect_prob = 1.0;
+		}
+
+		//Using Fresnel's equations, can get reflections occuring due to total internal reflection
+
+		if ((rand() / (RAND_MAX + 1.0)) < reflect_prob) {
+			scattered = ray(rec.p, reflected);
+		}
+		else {
+			scattered = ray(rec.p, refracted);
+		}
+		return true;
+	}
+
 };
