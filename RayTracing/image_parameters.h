2342b9eb9bf68d6bb7281405b5c76277f9ee8d29
Alex Richardson <alexander1.richardson@gmail.com>
2018-09-24 17:09:39 +0100

    Iterative rendering

--- RayTracing/image_parameters.h
+++ RayTracing/image_parameters.h
@@ -10,14 +10,25 @@
 //class chunk;
 
 struct image_parameters {
+
+	//Image dimensions
 	int nx;
 	int ny;
+
+	//Number of Samples
 	int ns;
 
+	//Output Storage
 	float *outputArray = { 0 };
 	float **outputArrayPtr = &outputArray;
 	std::list<chunk> chunkList;
 	std::list<chunk>::iterator iter;
+
+	//Iterative mode settings
+	bool iterative_mode = true;
+	int sample_number = 1;
+	int previous_samples = 0;
+
 
 };
 #endif //IMAGE_PARAMETERS_H
